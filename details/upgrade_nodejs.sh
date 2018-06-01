aa1fd796898821ef86af61d0f8c6dd88c3b1dd8c
FooFooDamon <udc577@126.com>
2018-06-01 16:28:42 +0800

    Added: details/upgrade_node_exe.sh: for upgrading nodejs conveniently

---
+++ details/upgrade_nodejs.sh
@@ -0,0 +1,5 @@
+sudo npm cache clean -f && sudo npm install n -g && sudo n stable && echo "NodeJS version after upgrade: $(node -v)"
+echo "If you find any problem, visit:"
+echo "  https://blog.csdn.net/tlbaba/article/details/79412433"
+echo "or use seach engines to look for the solution."
+
