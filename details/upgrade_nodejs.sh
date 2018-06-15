385ab8009e3aaa96be645223a7d744b046786fea
FooFooDamon <udc577@126.com>
2018-06-15 15:53:06 +0800

    Removed the npm automic installation

--- details/upgrade_nodejs.sh
+++ details/upgrade_nodejs.sh
@@ -8,15 +8,16 @@ usage()
 
 version()
 {
-	echo "$(basename $0): V1.00.00 2018/06/08"
+	# Removed the npm automic installation
+	echo "$(basename $0): V1.00.01 2018/06/15"
+
+	# Created.
+	#echo "$(basename $0): V1.00.00 2018/06/08"
 }
 
 source "$LAZY_SCRIPT_HOME"/import_lazy_script.sh
 
-is_installed || sudo $PROG_INSTALL npm
+is_installed npm || oops_quit "npm not installed! Please install it first and run this script again."
 
 sudo npm cache clean -f && sudo npm install n -g && sudo n stable && echo "NodeJS version after upgrade: $(node -v)"
-echo "If you find any problem, visit:"
-echo "    https://blog.csdn.net/tlbaba/article/details/79412433"
-echo "or use seach engines to look for the solution."
 
