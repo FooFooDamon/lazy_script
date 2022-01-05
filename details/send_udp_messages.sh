f8214f0acd60f38621a6c7ca2cbdb7e23a46060d
FooFooDamon <udc577@126.com>
2022-01-05 09:57:06 +0800

    details/send_{tcp,ucp}_messages.sh: V1.00.01: Use dynamic file descriptor.

--- details/send_udp_messages.sh
+++ details/send_udp_messages.sh
@@ -9,7 +9,8 @@ usage()
 
 version()
 {
-	echo "$(basename $0): V1.00.00 2018/06/08"
+	echo "$(basename $0): V1.00.01 2021/01/05" # Use dynamic file descriptor.
+	#echo "$(basename $0): V1.00.00 2018/06/08" # Create.
 }
 
 handle_sigPIPE()
@@ -30,13 +31,13 @@ ip=`echo "$1" | awk -F : '{ print $1 }'`
 port=`echo "$1" | awk -F : '{ print $2 }'`
 [ -n "$port" ] || oops_quit "Invalid address format, should be like: 127.0.0.1:8888"
 
-exec 9<> /dev/udp/$ip/$port || exit 1
+exec {fd}<> /dev/udp/$ip/$port || exit 1
 shift
 while [ $# -gt 0 ]
 do
 	current_msg="$1"
 	echo -e "Sending message to ${ip}:${port}: $current_msg"
-	echo -e "$current_msg" >&8
+	echo -e "$current_msg" >&${fd}
 	shift
 done
 
